1583d8b00bb374b1630cae0ab72fa5c8b04b4065
Cialyni <kvacheg1002@gmail.com>
2023-09-27 16:41:11 +0000

    black reformat

--- src/Homeworks/Homework_27.09/Task2.py
+++ src/Homeworks/Homework_27.09/Task2.py
@@ -7,7 +7,9 @@ def input_parser():
     file_txt_name = sys.argv[1]
     file_csv_name = sys.argv[2]
     script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-    return os.path.join(script_dir, file_txt_name), os.path.join(script_dir, file_csv_name)
+    return os.path.join(script_dir, file_txt_name), os.path.join(
+        script_dir, file_csv_name
+    )
 
 
 def frequency_find(file_txt_path):
@@ -24,9 +26,9 @@ def frequency_find(file_txt_path):
 
 
 def frequency_record(file_csv_path, frequency_dict):
-    file = open(file_csv_path, 'w')
+    file = open(file_csv_path, "w")
     for elem in frequency_dict:
-        file.write(str(elem) + ' ' + str(frequency_dict[elem]) + '\n')
+        file.write(str(elem) + " " + str(frequency_dict[elem]) + "\n")
 
 
 if __name__ == "__main__":
