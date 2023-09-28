a1451c663c6ed340e61be4753df5778fa44d5b01
Cialyni <kvacheg1002@gmail.com>
2023-09-28 23:01:10 +0000

    Reformat with black

--- src/Practices/Practice_22.09/Cows_and_Bulls.py
+++ src/Practices/Practice_22.09/Cows_and_Bulls.py
@@ -1,13 +1,21 @@
 import random
 
-def desclaimer():
-    print('Hi! In this game I will guess a four-digit number from non-repeating digits, you will try to guess it.\nEach attempt is one four-digit number, to which I will answer you how many cows you guessed (the numbers are not in their positions) and bulls (the numbers are in their positions.\nI give you exactly 5 attempts, otherwise you lose')
 
-def input_digit(attemp):
-    print('This is your attemp №:', attemp)
-    print('Input your four-digit number:')
+def disclaimer():
+    print(
+        "Hi! In this game I will guess a four-digit number from non-repeating digits, you will try to guess it.\n"
+        "Each attempt is one four-digit number, to which I will answer you how many cows you guessed"
+        " (the numbers are not in their positions) and bulls (the numbers are in their positions.\n"
+        "I give you exactly 5 attempts, otherwise you lose"
+    )
+
+
+def input_digit(attempt):
+    print("This is your attempt №:", attempt)
+    print("Input your four-digit number:")
     digit = input()
     return digit
+
 
 def check(my_digit, digit):
     cows = bulls = 0
@@ -21,19 +29,19 @@ def check(my_digit, digit):
 
 def game():
     my_digit = random.sample(range(10), 4)
-    attemp = 1
-    while attemp <= 5:
-        digit = input_digit(attemp)
+    attempt = 1
+    while attempt <= 5:
+        digit = input_digit(attempt)
         cows, bulls = check(my_digit, digit)
         if bulls != 4:
-            print('Cows count: {}\nBulls count: {}'.format(cows, bulls))
+            print("Cows count: {}\nBulls count: {}".format(cows, bulls))
         else:
-            print('Congratulation!!!!\nYou WIN')
+            print("Congratulation!!!!\nYou WIN")
             return
-        attemp += 1
-    print('You lose, try better next time')
+        attempt += 1
+    print("You lose, try better next time")
 
 
-if __name__ == '__main__':
-    desclaimer()
+if __name__ == "__main__":
+    disclaimer()
     game()
