090adee5d0c9c81b60c10388ecfee638f220a9ef
Cialyni <kvacheg1002@gmail.com>
2023-10-04 18:09:01 +0000

    change after codereview

--- src/Homeworks/Homework_27.09/Task2.py
+++ src/Homeworks/Homework_27.09/Task2.py
@@ -3,32 +3,27 @@ import sys
 
 
 def input_parser():
-    print(sys.argv)
-    file_txt_name = sys.argv[1]
-    file_csv_name = sys.argv[2]
-    script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-    return os.path.join(script_dir, file_txt_name), os.path.join(
-        script_dir, file_csv_name
-    )
+    file_txt = sys.argv[1]
+    file_csv = sys.argv[2]
+    script_path = os.path.dirname(__file__)
+    return os.path.join(script_path, file_txt), os.path.join(script_path, file_csv)
 
 
-def frequency_find(file_txt_path):
+def frequency_find(file_txt):
     word_dict = {}
-    with open(file_txt_path) as f_in:
+    with open(file_txt) as f_in:
         for line in f_in:
             word_lst = line.split()
             for word in word_lst:
-                if word in word_dict:
-                    word_dict[word] += 1
-                else:
-                    word_dict[word] = 1
+                word_dict = word_dict.get(word, 0) + 1
     return word_dict
 
 
-def frequency_record(file_csv_path, frequency_dict):
-    file = open(file_csv_path, "w")
+def frequency_record(file_csv, frequency_dict):
+    file = open(file_csv, "w")
     for elem in frequency_dict:
         file.write(str(elem) + " " + str(frequency_dict[elem]) + "\n")
+    file.close()
 
 
 if __name__ == "__main__":
