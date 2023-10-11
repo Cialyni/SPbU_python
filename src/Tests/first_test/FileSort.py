4aedb5641f9e60b0a49d63fdb518c76a14201732
Cialyni <kvacheg1002@gmail.com>
2023-10-11 14:36:20 +0300

    add output_file fuction and file_g exist check

--- src/Tests/first_test/FileSort.py
+++ src/Tests/first_test/FileSort.py
@@ -13,25 +13,29 @@ def error_executor(f, g):
     if not os.path.isfile(file_f):
         print("File f doesn't exist")
         sys.exit()
-    """if os.path.isfile(file_g):
+    if os.path.isfile(file_g):
         print("File g already exist")
-        sys.exit()"""
+        sys.exit()
 
 
-def file_sorter(a, b, file_f, file_g):
-    answer_lst = [[], [], []]
+def file_sorter(a, b, file_f):
+    sorted_file_data = [[], [], []]
     with open(file_f) as f_in:
         for line in f_in:
             for number in line.split():
                 if int(number) < a:
-                    answer_lst[0].append(number)
+                    sorted_file_data[0].append(number)
                 elif a <= int(number) <= b:
-                    answer_lst[1].append(number)
+                    sorted_file_data[1].append(number)
                 else:
-                    answer_lst[2].append(number)
-    g = open(file_g, "w")
+                    sorted_file_data[2].append(number)
+    return sorted_file_data
+
+
+def output_file(lst, file):
+    g = open(file, "w")
     for i in range(3):
-        for elem in answer_lst[i]:
+        for elem in lst[i]:
             g.write(elem + " ")
         g.write("\n")
     g.close()
@@ -40,4 +44,5 @@ def file_sorter(a, b, file_f, file_g):
 if __name__ == "__main__":
     a, b, file_f, file_g = input_parser()
     error_executor(file_f, file_g)
-    file_sorter(a, b, file_f, file_g)
+    sorted_file_data = file_sorter(a, b, file_f)
+    output_file(sorted_file_data, file_g)
