9d2a33fcf07aa1fc1212b35a0330420d590d2d3c
Cialyni <kvacheg1002@gmail.com>
2023-10-26 16:13:14 +0300

    add more exception tests, rewrite is_float_func into parce_user_input, make some little changes

--- src/practices/practice_6/task1.py
+++ src/practices/practice_6/task1.py
@@ -1,59 +1,54 @@
-def is_float_numbers(s):
+def parse_user_input(s):
     flag = True
     try:
-        for i in s.split():
-            float(i)
+        s = s.split()
+        float(s[0]), float(s[1]), float(s[2])
     except ValueError:
         flag = False
+        raise ValueError("Need 3 float arguments in one string with space: A B C")
     finally:
-        if len(s.split()) == 3:
-            return True and flag
-        return False
+        if len(s) == 3 and flag:
+            return float(s[0]), float(s[1]), float(s[2])
+        else:
+            raise ValueError("Need 3 float arguments in one string with space: A B C")
 
 
 def inputer():
-    print(
-        "to solve an equation Ax^2 + Bx + C = 0\nEnter the coefficients A, B, C in 1 line"
+    s = input(
+        "to solve an equation Ax^2 + Bx + C = 0\nEnter the coefficients A, B, C in 1 line\n"
     )
-    s = input()
-    if is_float_numbers(s):
-        a, b, c = float(s.split()[0]), float(s.split()[1]), float(s.split()[2])
-        return a, b, c
-    raise KeyboardInterrupt("Need 3 float arguments in one string with space: A B C")
+    a, b, c = parse_user_input(s)
+    return a, b, c
 
 
 def linear_equation_solve(k, b):  # kx + b = 0
-    x = None
-    try:
-        x = -b / k
-    except ZeroDivisionError:
-        if k == b:
-            raise ValueError("This equation has infinity counts of roots")
-        raise ZeroDivisionError("this equation is not linear")
-    finally:
-        return x
+    if not k and not b:
+        raise ValueError("infinite number of solutions with k = b = 0")
+    elif not k and b:
+        raise ZeroDivisionError("division by zero: k = 0")
+    else:
+        return -b / k
 
 
-def quadratic_equation_solve(A, B, C):  # Ax^2 + Bx + C = 0
+def quadratic_equation_solve(a, b, c):  # Ax^2 + Bx + C = 0
     x1, x2 = None, None
-    D = B * B - 4 * A * C
+    D = b * b - 4 * a * c
+    if a == 0:
+        return (linear_equation_solve(b, c),)
     if D < 0:
         raise ValueError("Negative discriminant, can not solve it")
-    try:
-        x1 = (-B - D**0.5) / (2 * A)
-        x2 = (-B + D**0.5) / (2 * A)
-        if D == 0:
-            x2 = None
-    except ZeroDivisionError:
-        x1 = linear_equation_solve(B, C)
-    finally:
-        return x1, x2
+    if D == 0:
+        return (-b / (2 * a),)
+    if D > 0:
+        x1 = (-b - D**0.5) / (2 * a)
+        x2 = (-b + D**0.5) / (2 * a)
+        return min(x1, x2), max(x1, x2)
 
 
 def main():
     a, b, c = inputer()
-    x1, x2 = quadratic_equation_solve(a, b, c)
-    return x1, x2
+    ans = quadratic_equation_solve(a, b, c)
+    return ans
 
 
 if __name__ == "__main__":
--- tests/practices/practice_6/test_task1.py
+++ tests/practices/practice_6/test_task1.py
@@ -3,7 +3,7 @@ from unittest import mock
 from src.practices.practice_6.task1 import (
     quadratic_equation_solve,
     linear_equation_solve,
-    is_float_numbers,
+    parse_user_input,
     main,
 )
 
@@ -12,13 +12,16 @@ from src.practices.practice_6.task1 import (
     "a, b, c, expected",
     (
         (1, 2, -3, (-3, 1)),
-        (1, 2, 1, (-1, None)),
-        (0, 1, 5, (-5, None)),
+        (1, 2, 1, (-1,)),
+        (0, 1, 5, (-5,)),
     ),
 )
 def test_quadratic_equation(a, b, c, expected):
     actual = quadratic_equation_solve(a, b, c)
-    assert actual[0] == expected[0] and actual[1] == expected[1]
+    if len(actual) == 2:
+        assert actual[0] == expected[0] and actual[1] == expected[1]
+    else:
+        assert actual[0] == expected[0]
 
 
 def test_exception_in_quadratic_equation(a=1, b=2, c=10):
@@ -26,34 +29,55 @@ def test_exception_in_quadratic_equation(a=1, b=2, c=10):
         quadratic_equation_solve(a, b, c)
 
 
-@pytest.mark.parametrize(
-    "k, b, expected", ((12, 54, (-54 / 12)), (0, 0, None), (0, 5, None), (5, 0, 0))
-)
+@pytest.mark.parametrize("k, b, expected", ((12, 54, (-54 / 12)), (5, 0, 0)))
 def test_linear_equation(k, b, expected):
     actual = linear_equation_solve(k, b)
     assert actual == expected
 
 
+def test_ZeroDivisionError_exception_in_quadratic_equation(b=0, k=5):
+    with pytest.raises(ZeroDivisionError, match="division by zero: k = 0"):
+        linear_equation_solve(b, k)
+
+
+def test_ValueError_exception_in_quadratic_equation(b=0, k=0):
+    with pytest.raises(ValueError, match="infinite number of solutions with k = b = 0"):
+        linear_equation_solve(b, k)
+
+
 @pytest.mark.parametrize(
     "lst, expected",
     (
-        ("1 2 3", True),
-        (" ", False),
-        (" 24.2  11    0", True),
-        (" 24.2  11. .0", True),
-        (" 24..2  11. 0", False),
-        ("adf 1 dl-dd", False),
-        ("11.1", False),
-        ("1 2 3 4", False),
-        ("1,13 4,3 1", False),
-        ("1e3 1.133e-5 1", True),
+        ("1 2 3", (1, 2, 3)),
+        (" 24.2  11    0", (24.2, 11, 0)),
+        (" 24.2  11. .0", (24.2, 11.0, 0.0)),
+        ("1e3 1.133e-5 1", (1e3, 1.133e-5, 1)),
     ),
 )
-def test_is_float_numbers(lst, expected):
-    actual = is_float_numbers(lst)
+def test_parse_user_input(lst, expected):
+    actual = parse_user_input(lst)
     assert actual == expected
 
 
+@pytest.mark.parametrize(
+    "s",
+    (
+        " 24..2  11. 0",
+        "adf 1 dl-dd",
+        "11.1",
+        "1 2 3 4",
+        "1,13 4,3 1",
+        " ",
+        "adf 1 dl-dd",
+    ),
+)
+def test_ValueError_exception_in_parse_user_input(s):
+    with pytest.raises(
+        ValueError, match="Need 3 float arguments in one string with space: A B C"
+    ):
+        parse_user_input(s)
+
+
 def test_main():
-    with mock.patch.object(builtins, "input", lambda: "1 4 3"):
+    with mock.patch.object(builtins, "input", lambda _: "1 4 3"):
         assert main() == (-3.0, -1.0)
