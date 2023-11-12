3d2b8623cc8ee00a986bfae979803db84b52e506
Cialyni <kvacheg1002@gmail.com>
2023-11-12 19:10:46 +0300

    rename some variables, changed 'int(not int(elem))' constuction, make small fixes

--- src/Homeworks/Homework_18.10/Binary_Calculator.py
+++ src/Homeworks/Homework_18.10/Binary_Calculator.py
@@ -5,17 +5,25 @@ def to_decimal(x):
             ans += int(x[i]) * (2**i)
     else:
         x = bin_sum(x, "111")  # -1 in decimal
-        x = [int(not int(i)) for i in x]
+        x = [1 if i == "0" else 0 for i in x]
         for i in range(len(x)):
             ans += x[i] * (2**i)
         ans *= -1
     return ans
 
 
-def bin_normalization(x, y):
-    x = x + x[-1] * (len(y) - len(x)) if len(x) < len(y) else x
-    y = y + y[-1] * (len(x) - len(y)) if len(x) > len(y) else y
-    return x, y
+def bin_normalization(first_binary, second_binary):
+    first_binary = (
+        first_binary + first_binary[-1] * (len(second_binary) - len(first_binary))
+        if len(first_binary) < len(second_binary)
+        else first_binary
+    )
+    second_binary = (
+        second_binary + second_binary[-1] * (len(first_binary) - len(second_binary))
+        if len(first_binary) > len(second_binary)
+        else second_binary
+    )
+    return first_binary, second_binary
 
 
 def bin_sum(x, y):
@@ -24,11 +32,12 @@ def bin_sum(x, y):
     bit = 0
     for i in range(len(x)):
         xi, yi = int(x[i]), int(y[i])
+        res = (xi + yi + bit) % 2
         if xi + yi + bit >= 2:
-            ans += str((xi + yi + bit) % 2)
+            ans += str(res)
             bit = 1
         else:
-            ans += str((xi + yi + bit) % 2)
+            ans += str(res)
             bit = 0
     if len(ans) == len(x):
         ans += ans[-1]
@@ -44,11 +53,11 @@ def to_direct_code(x):
     return bin_x
 
 
-def to_additional_code(x):
-    x = [int(not int(elem)) for elem in x]
-    x += "11"
-    x = bin_sum(x, "100")  # 1 in decimal
-    return x
+def to_additional_code(binary_x):
+    binary_x = ["1" if elem == "0" else "0" for elem in binary_x]
+    binary_x += "11"
+    binary_x = bin_sum(binary_x, "100")  # 1 in decimal
+    return binary_x
 
 
 def to_binary(x):
@@ -62,14 +71,12 @@ def to_binary(x):
 
 if __name__ == "__main__":
     try:
-        a, b = int(input("Enter 1-st decimal number")), int(
-            input("Enter 2-nd decimal number:")
-        )
+        a = int(input("Enter 1-st decimal number"))
+        b = int(input("Enter 2-nd decimal number:"))
+    except ValueError:
+        print("Input numbers!")
+    finally:
         bin_a, bin_b, bin_negative_b = to_binary(a), to_binary(b), to_binary(-b)
         print(  # f-string so cool 0_0. Why I don't use them early???
             f"{a} + {b} = {to_decimal(bin_sum(bin_a, bin_b))}\n{a} - {b} = {to_decimal(bin_sum(bin_a, bin_negative_b))}"
         )
-    except ValueError:
-        print("Input numbers!")
-    finally:
-        print("End of program")
