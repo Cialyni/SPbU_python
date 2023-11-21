7e778b66f056c18c45926d14c29aeec3bcbcd109
Cialyni <kvacheg1002@gmail.com>
2023-11-21 23:42:39 +0300

    initialization commit

---
+++ src/Homeworks/Homework_6/AVL_Tree.py
@@ -0,0 +1,254 @@
+from typing import TypeVar, Generic, Any
+from dataclasses import dataclass
+
+T = TypeVar("T")
+REMOVING_VALUE = None
+
+
+@dataclass
+class TreeNode(Generic[T]):
+    key: Any
+    value: Any
+    left: "TreeNode[T]"
+    right: "TreeNode[T]"
+    height: int = 1
+
+
+@dataclass
+class Tree(Generic[T]):
+    root: TreeNode[T] = None
+
+
+def create_tree_map() -> Tree:
+    return Tree()
+
+
+def delete_tree_map(map: Tree):
+    def rec_delete(root: TreeNode):
+        if not (root is None):
+            rec_delete(root.left)
+            rec_delete(root.right)
+            del root
+
+    map.root = rec_delete(map.root)
+
+
+def _balance(node: TreeNode) -> int:
+    return 0 if node is None else _height(node.left) - _height(node.right)
+
+
+def _height(node: TreeNode) -> int:
+    return 0 if node is None else node.height
+
+
+def rotateL(node: TreeNode) -> TreeNode:
+    a = node.right
+    b = a.left
+    a.left = node
+    node.right = b
+    node.height = 1 + max(_height(node.left), _height(node.right))
+    a.height = 1 + max(_height(a.left), _height(a.right))
+    return a
+
+
+def rotateR(node: TreeNode) -> TreeNode:
+    a = node.left
+    b = a.right
+    a.right = node
+    node.left = b
+    node.height = 1 + max(_height(node.left), _height(node.right))
+    a.height = 1 + max(_height(a.left), _height(a.right))
+    return a
+
+
+def put(map: Tree, key: Any, value: Any):
+    if map.root is None:
+        map.root = TreeNode(key, value, None, None, 1)
+    else:
+        map.root = insert(map.root, key, value)
+
+
+def insert(root: TreeNode, key: Any, value: Any):
+    if root is None:
+        return TreeNode(key, value, None, None, 1)
+    if key > root.key:
+        root.right = insert(root.right, key, value)
+    elif key < root.key:
+        root.left = insert(root.left, key, value)
+    else:
+        root.value = value
+    root.height = 1 + max(_height(root.left), _height(root.right))
+    balance = _balance(root)
+    if balance > 1 and root.left.key > key:
+        return rotateR(root)
+    if balance < -1 and key > root.right.key:
+        return rotateL(root)
+    if balance > 1 and key > root.left.key:
+        root.left = rotateL(root.left)
+        return rotateR(root)
+    if balance < -1 and key < root.right.key:
+        root.right = rotateR(root.right)
+        return rotateL(root)
+    return root
+
+
+def get(map: Tree, key: Any) -> Any:
+    current_root = map.root
+    while not (current_root is None):
+        if key == current_root.key:
+            return current_root.value
+        elif key > current_root.key:
+            current_root = current_root.right
+        else:
+            current_root = current_root.left
+    raise AttributeError("BST hasn't Node with this key")
+
+
+def has_key(map: Tree, key: Any) -> bool:
+    current_root = map.root
+    while not (current_root is None):
+        if key == current_root.key:
+            return True
+        elif key > current_root.key:
+            current_root = current_root.right
+        elif key < current_root.key:
+            current_root = current_root.left
+    return False
+
+
+def get_maximum(map: Tree) -> Any:
+    current_root = map.root
+    maximum_key = current_root.key
+    while not current_root is None:
+        maximum_key = current_root.key
+        current_root = current_root.right
+    return maximum_key
+
+
+def get_minimum(map: Tree) -> Any:
+    current_root = map.root
+    minimum_key = current_root.key
+    while not current_root is None:
+        minimum_key = current_root.key
+        current_root = current_root.left
+    return minimum_key
+
+
+def get_upper_bound(map: Tree, key: Any) -> Any:
+    if get_maximum(map) < key:
+        raise AttributeError
+    current_root = map.root
+    upper_bound = current_root.key
+    while not (current_root is None):
+        upper_bound = current_root.key if current_root.key > key else upper_bound
+        if current_root.key > key:
+            current_root = current_root.left
+        else:
+            current_root = current_root.right
+    return upper_bound
+
+
+def get_lower_bound(map: Tree, key: Any) -> Any:
+    if has_key(map, key):
+        return key
+    return get_upper_bound(map, key)
+
+
+def find_min_in_right_subtree(root: TreeNode) -> TreeNode:
+    current_root = root.right
+    if current_root is None:
+        return root
+    while not (current_root.left is None):
+        current_root = current_root.left
+    return current_root
+
+
+def remove(map: Tree, key: Any) -> Any:
+    if not has_key(map, key):
+        raise AttributeError("BST hasn't Node with this key")
+    deleting_value = get(map, key)
+
+    def _removing(current_root: TreeNode, key: Any):
+        if current_root.key > key:
+            current_root.left = _removing(current_root.left, key)
+        elif current_root.key < key:
+            current_root.right = _removing(current_root.right, key)
+        else:
+            if current_root.left is None:
+                return current_root.right
+            elif current_root.right is None:
+                return current_root.left
+
+            new_node = find_min_in_right_subtree(current_root)
+            current_root.key, current_root.value = new_node.key, new_node.value
+            current_root.right = _removing(current_root.right, current_root.key)
+        current_root.height = 1 + max(_height(current_root.left), _height(current_root.right))
+        balance = _balance(current_root)
+        if balance > 1 and _balance(current_root.left) >= 0:
+            return rotateR(current_root)
+        if balance < -1 and _balance(current_root.right) <= 0:
+            return rotateL(current_root)
+        if balance > 1 and _balance(current_root.left) < 0:
+            current_root.left = rotateL(current_root.left)
+            return rotateR(current_root)
+        if balance < -1 and _balance(current_root.right) > 0:
+            current_root.right = rotateR(current_root.right)
+            return rotateL(current_root)
+        return current_root
+
+    map.root = _removing(map.root, key)
+    return deleting_value
+
+
+def pre_order_traverse(root: TreeNode, nodes):
+    if not (root is None):
+        nodes.append(root.key)
+        pre_order_traverse(root.left, nodes)
+        pre_order_traverse(root.right, nodes)
+    return nodes
+
+
+def in_order_traverse(root: TreeNode, nodes):
+    if not (root is None):
+        in_order_traverse(root.left, nodes)
+        nodes.append(root.key)
+        in_order_traverse(root.right, nodes)
+    return nodes
+
+
+def post_order_traverse(root: TreeNode, nodes):
+    if not (root is None):
+        post_order_traverse(root.left, nodes)
+        post_order_traverse(root.right, nodes)
+        nodes.append(root.key)
+    return nodes
+
+
+def traverse(map: Tree, order: str) -> list[T]:
+    ans = []
+    if order == "pre-order":
+        return pre_order_traverse(map.root, ans)
+    if order == "in-order":
+        return in_order_traverse(map.root, ans)
+    if order == "post-order":
+        return post_order_traverse(map.root, ans)
+
+
+# ______________________________________________________________________________
+
+if __name__ == "__main__":
+    bst = create_tree_map()
+    put(bst, 7, 1)
+    put(bst, 8, 2)
+    put(bst, 13, 3)
+    put(bst, 3, 4)
+    put(bst, 9, 5)
+    put(bst, 1, 6)
+    put(bst, 4, 111)
+    put(bst, 6, -1)
+    put(bst, 4, 'ad')
+    put(bst, 2, '12122')
+    put(bst, -5,  [12, 2, 3])
+    put(bst, 11, '1')
+    put(bst, 15, (-12, 3))
+    print(traverse(bst, 'post-order'))
---
+++ tests/Homeworks/Homework_6/test_AVL_Tree.py
@@ -0,0 +1,130 @@
+import pytest
+from src.Homeworks.Homework_6.AVL_Tree import (
+    TreeNode,
+    Tree,
+    put,
+    get,
+    has_key,
+    remove,
+    create_tree_map,
+    get_maximum,
+    get_lower_bound,
+    get_minimum,
+    get_upper_bound,
+    traverse
+
+)
+
+TEST_AVL_TREE = Tree(root=TreeNode(key=8, value=2, left=TreeNode(key=3, value=4, left=TreeNode(key=1, value=6,
+                                                                                               left=TreeNode(key=-5,
+                                                                                                             value=[12,
+                                                                                                                    2,
+                                                                                                                    3],
+                                                                                                             left=None,
+                                                                                                             right=None,
+                                                                                                             height=1),
+                                                                                               right=TreeNode(key=2,
+                                                                                                              value='12122',
+                                                                                                              left=None,
+                                                                                                              right=None,
+                                                                                                              height=1),
+                                                                                               height=2),
+                                                                 right=TreeNode(key=6, value=-1,
+                                                                                left=TreeNode(key=4, value='ad',
+                                                                                              left=None, right=None,
+                                                                                              height=1),
+                                                                                right=TreeNode(key=7, value=1,
+                                                                                               left=None, right=None,
+                                                                                               height=1), height=2),
+                                                                 height=3), right=TreeNode(key=11, value='1',
+                                                                                           left=TreeNode(key=9, value=5,
+                                                                                                         left=None,
+                                                                                                         right=None,
+                                                                                                         height=1),
+                                                                                           right=TreeNode(key=13,
+                                                                                                          value=3,
+                                                                                                          left=None,
+                                                                                                          right=TreeNode(
+                                                                                                              key=15,
+                                                                                                              value=(
+                                                                                                                  -12,
+                                                                                                                  3),
+                                                                                                              left=None,
+                                                                                                              right=None,
+                                                                                                              height=1),
+                                                                                                          height=2),
+                                                                                           height=3), height=4))
+
+
+def test_exception_in_get(key1=212, key2=0):
+    with pytest.raises(AttributeError, match="BST hasn't Node with this key"):
+        get(TEST_AVL_TREE, key1)
+        get(TEST_AVL_TREE, key2)
+
+
+def test_exception_in_remove(key1=212, key2=-1):
+    with pytest.raises(AttributeError, match="BST hasn't Node with this key"):
+        remove(TEST_AVL_TREE, key1)
+        remove(TEST_AVL_TREE, key2)
+
+
+def test_exception_in_lower_and_upper_bound(key1=212, key2=17):
+    with pytest.raises(AttributeError):
+        get_upper_bound(TEST_AVL_TREE, key1)
+        get_lower_bound(TEST_AVL_TREE, key2)
+
+
+def test_create_hash_table():
+    assert type(create_tree_map()) == Tree
+
+
+@pytest.mark.parametrize(
+    "key, expected",
+    (
+            (13, True),
+            (-2, False),
+            (122, False),
+    ),
+)
+def test_has_key(key, expected):
+    assert has_key(TEST_AVL_TREE, key) == expected
+
+
+@pytest.mark.parametrize(
+    "key, expected",
+    (
+            (-5, [12, 2, 3]),
+            (4, 'ad'),
+    ),
+)
+def test_get(key, expected):
+    assert get(TEST_AVL_TREE, key) == expected
+
+
+def test_put(key1=212, key2=0, value1="212", value2=1412):
+    ht = create_tree_map()
+    put(ht, key1, value1)
+    put(ht, key2, value2)
+    assert (
+            has_key(ht, key1)
+            and has_key(ht, key2)
+            and get(ht, key1) == value1
+            and get(ht, key2) == value2
+    )
+
+
+@pytest.mark.parametrize(
+    "order, expected",
+    (
+            ('pre-order', [8, 3, 1, -5, 2, 6, 4, 7, 11, 9, 13, 15]),
+            ('in-order', [-5, 1, 2, 3, 4, 6, 7, 8, 9, 11, 13, 15]),
+            ('post-order', [-5, 2, 1, 4, 7, 6, 3, 9, 15, 13, 11, 8])
+    ),
+)
+def test_traverse(order, expected):
+    if order == 'pre-order':
+        assert traverse(TEST_AVL_TREE, 'pre-order') == expected
+    if order == 'in-order':
+        assert traverse(TEST_AVL_TREE, 'in-order') == expected
+    if order == 'post-order':
+        assert traverse(TEST_AVL_TREE, 'post-order') == expected
