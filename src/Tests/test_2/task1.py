bf69576ae150dba72806dbfdcddb9fc01a4dbdae
Cialyni <kvacheg1002@gmail.com>
2023-11-24 14:18:26 +0300

    initial commit in task1

---
+++ src/Tests/test_2/task1.py
@@ -0,0 +1,26 @@
+def fib_number_finder(n: int) -> int:
+    if 0 <= n <= 1:
+        return 1
+    elif 1 < n <= 90:
+        f, s = 1, 1
+        output_num = 1
+        num = 1
+        while num < n:
+            output_num = f + s
+            f = s
+            s = output_num
+            num += 1
+        return output_num
+    raise ValueError
+
+
+def main():
+    try:
+        n = int(input('Input n and program will find the Fibonacci number with the index n:\n'))
+        print(f'Result is: {fib_number_finder(n)}')
+    except ValueError:
+        print('ERROR!\nEnter only one integer number in range [0:90]')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/Tests/test_2/test_task1.py
@@ -0,0 +1,49 @@
+import pytest
+from io import StringIO
+from src.Tests.test_2.task1 import (
+    fib_number_finder,
+    main
+)
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    (
+        (13, 377),
+        (66, 44945570212853),
+    ),
+)
+def test_fib_number_finder(n, expected):
+    assert fib_number_finder(n) == expected
+
+
+def test_exception_in_fib_number_finder():
+    with pytest.raises(
+        ValueError,
+    ):
+        fib_number_finder(-1)
+        fib_number_finder(91)
+        fib_number_finder('ase')
+
+
+
+@pytest.mark.parametrize(  # this strange format made black :)
+    "imitation_input, expected",
+    (
+        (
+            "-1",
+            "ERROR!\nEnter only one integer number in range [0:90]",
+        ),
+        ("0 0 0", "ERROR!\nEnter only one integer number in range [0:90]"),
+        ("abasdasf", "ERROR!\nEnter only one integer number in range [0:90]"),
+        ("45", "Result is: 1836311903"),
+        ("90", "Result is: 4660046610375530309"),
+    ),
+)
+def test_main(monkeypatch, imitation_input, expected):
+    monkeypatch.setattr("builtins.input", lambda _: imitation_input)
+    imitation_output = StringIO()
+    monkeypatch.setattr("sys.stdout", imitation_output)
+    main()
+    output = imitation_output.getvalue()
+    assert output == expected + "\n"
