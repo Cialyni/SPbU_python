6ed6ab47b925366bcb4ced8b58298431aa014171
Cialyni <kvacheg1002@gmail.com>
2023-12-22 20:24:34 +0300

    separate return test function into two function: with warnings and without

--- tests/Tests/final_rewrite/test_safe_call.py
+++ tests/Tests/final_rewrite/test_safe_call.py
@@ -6,24 +6,35 @@ from src.Tests.final_rewirte.safe_call import safe_call
 @pytest.mark.parametrize(
     "func, expected",
     (
-        (lambda x="aboba": int(x), None),
-        (lambda: int(1 / 0), None),
-        (lambda x="prnt(int(1))": eval(x), None),
         (lambda x="int(13)": eval(x), 13),
         (lambda x=5: x / 10, 0.5),
+        (lambda x=0: x + 1, 1),
     ),
 )
-def test_return_with_safe_call(func, expected):
+def test_return_without_warning(func, expected):
     func = safe_call(func)
     assert func() == expected
 
 
+@pytest.mark.parametrize(
+    "func, expected",
+    (
+        (lambda x="aboba": int(x), None),
+        (lambda: int(1 / 0), None),
+        (lambda x="prnt(int(1))": eval(x), None),
+    ),
+)
+def test_return_with_warning(func, expected):
+    with pytest.warns(UserWarning):
+        assert safe_call(func)() == expected
+
+
 def test_checking_warning_cast():
-    with pytest.warns(Warning):
+    with pytest.warns(UserWarning):
         safe_call(lambda: int(1 / 0))()
-    with pytest.warns(Warning):
+    with pytest.warns(UserWarning):
         safe_call(lambda x="aboba": int(x))()
-    with pytest.warns(Warning):
+    with pytest.warns(UserWarning):
         safe_call(lambda x="prnt(int(1))": eval(x))()
 
 
