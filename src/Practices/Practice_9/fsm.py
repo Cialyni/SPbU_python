de02f6e4fb6623e7639ad51bfb2647aa9bb94001
Kvachev George <144917849+Cialyni@users.noreply.github.com>
2023-12-27 16:54:47 +0300

    Kvachev. Homework 7 (#30)
    * initial commit
    
    * fixed import error
    
    * delete usless typing, change fsm_create funcion
    
    * add homework part (second fsm)
    
    * change the token of the transition to the list of tokens. removed the manual language check
    
    * add for/else, add create_float_fs_machine
    
    * do black
    
    * black again :)
    
    * Update test_fsm.py
    
    * Update test_fsm.py
    
    * Update test_fsm.py
    
    * Update test_fsm.py
    
    ---------
    
    Co-authored-by: Egor Spirin <Spirin.egor@gmail.com>

--- src/Practices/Practice_9/fsm.py
+++ src/Practices/Practice_9/fsm.py
@@ -1,39 +1,73 @@
 from dataclasses import dataclass
+import string
 from typing import Any
 
 
 @dataclass
 class FSMachine:
+    language_name: str
     initial: int
-    accepting: list[int]
+    accepting: tuple[int]
     transitions: dict[int, dict[Any, int]]
 
 
-def create_fs_machine(initial, accepting, transitions) -> FSMachine:
-    return FSMachine(initial, accepting, transitions)
-
-
-def create_aboba_fs_machine():
-    fsm = create_fs_machine(
-        initial=0,
-        accepting=[
-            3,
-        ],
-        transitions={
-            0: {"a": 1, "b": 0},
-            1: {"a": 1, "b": 2},
-            2: {"a": 1, "b": 3},
-            3: {"a": 1, "b": 0},
-        },
-    )
-    return fsm
+def create_fs_machine(language_name, initial, accepting, transitions) -> FSMachine:
+    return FSMachine(language_name, initial, accepting, transitions)
 
 
 def validate_string(fsm: FSMachine, string: str) -> bool:
     current_condition = fsm.initial
     for token in string:
-        if token in fsm.transitions[current_condition]:
-            current_condition = fsm.transitions[current_condition][token]
+        for transition in fsm.transitions[current_condition].keys():
+            if token in transition:
+                current_condition = fsm.transitions[current_condition][transition]
+                break
         else:
             return False
+
     return current_condition in fsm.accepting
+
+
+def create_aboba_fs_machine():
+    FSM_aboba = create_fs_machine(
+        language_name="(a|b)*abb",
+        initial=0,
+        accepting=(0, 4),
+        transitions={
+            0: {"a": 2, "b": 1},
+            1: {"a": 2, "b": 1},
+            2: {"a": 2, "b": 3},
+            3: {"a": 2, "b": 4},
+            4: {"a": 2, "b": 1},
+        },
+    )
+    return FSM_aboba
+
+
+def create_float_fs_machine():
+    FSM_float = create_fs_machine(
+        language_name="digit+(.digit+)?(E(+|-)?digit+)?",
+        initial=0,
+        accepting=(0, 1, 5),
+        transitions={
+            0: {string.digits: 1},
+            1: {string.digits: 1, "E": 3, ".": 2},
+            2: {string.digits: 1},
+            3: {string.digits: 5, "+-": 4},
+            4: {string.digits: 5},
+            5: {string.digits: 5},
+        },
+    )
+    return FSM_float
+
+
+def give_message_about_string(fsm_lst: list[FSMachine], string: str):
+    output_str = ""
+    if string == "":
+        return "Еhe empty set is suitable for all languages\n"
+    for fsm in fsm_lst:
+        if validate_string(fsm, string):
+            output_str += "This string belong to the " + fsm.language_name + "\n"
+    if not output_str:
+        return "This string does not belong to the any of given languages\n"
+    return output_str
--- tests/Practices/Practice_9/test_fsm.py
+++ tests/Practices/Practice_9/test_fsm.py
@@ -1,27 +1,38 @@
 import pytest
 from io import StringIO
 
-from src.Practices.Practice_9.fsm import validate_string, create_aboba_fs_machine
+from src.Practices.Practice_9.fsm import (
+    give_message_about_string,
+    create_aboba_fs_machine,
+    create_float_fs_machine,
+    validate_string,
+)
 from src.Practices.Practice_9.main import main
 
-
-FSM = create_aboba_fs_machine()
+FSM_aboba = create_aboba_fs_machine()
+FSM_float = create_float_fs_machine()
 
 
 @pytest.mark.parametrize(
     "string, expected",
     (
-        ("", False),
         ("abb", True),
-        ("ababababbbbbaaabb", True),
+        ("123.1222", True),
         ("abbabbabbabb", True),
         ("abababeedfsseabb", False),
         ("abbab", False),
-        ("sefafeczvcnny", False),
+        ("1E32", True),
+        ("1++32.22E", False),
+        ("", True),
+        ("12.25E+0049", True),
+        ("12.25E+0049E-2324", False),
     ),
 )
 def test_validate_string(string, expected):
-    assert validate_string(FSM, string) == expected
+    assert (
+        validate_string(FSM_float, string) == expected
+        or validate_string(FSM_aboba, string) == expected
+    )
 
 
 @pytest.mark.parametrize(
