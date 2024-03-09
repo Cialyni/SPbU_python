0ed10d654dd6f8deb546587b80a05f7f76950438
Cialyni <kvacheg1002@gmail.com>
2024-03-09 22:45:12 +0300

    task4: Robot

---
+++ src/LeetCode/leetcode_task4.py
@@ -0,0 +1,42 @@
+from typing import List, Tuple
+
+
+def robot_move(
+    current_pos: List[int],
+    move_orientation: Tuple[int],
+    obstacles: Tuple[Tuple[int]],
+    k: int,
+):
+    x, y = current_pos[0], current_pos[1]
+    for i in range(k):
+        x += move_orientation[0]
+        y += move_orientation[1]
+        if (x, y) in obstacles:
+            return x - move_orientation[0], y - move_orientation[1]
+    return x, y
+
+
+def robot_sim(commands: List[int], obstacles: List[List[int]]) -> int:
+    ans = 0
+    current_pos = [0, 0]  # x, y
+    move_orientations = ((0, 1), (1, 0), (0, -1), (-1, 0))
+    obstacles = set((a, b) for a, b in obstacles)
+    current_orientation_ind = 0
+    for command in commands:
+        if command == -2:
+            current_orientation_ind = (current_orientation_ind - 1) % len(
+                move_orientations
+            )
+        elif command == -1:
+            current_orientation_ind = (current_orientation_ind + 1) % len(
+                move_orientations
+            )
+        else:
+            current_pos = robot_move(
+                current_pos,
+                move_orientations[current_orientation_ind],
+                obstacles,
+                command,
+            )
+            ans = max(ans, current_pos[0] ** 2 + current_pos[1] ** 2)
+    return ans
