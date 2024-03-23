a648fa44bdc36eb3b1e608f9d4e6c961fecf404c
Cialyni <kvacheg1002@gmail.com>
2024-03-23 20:16:26 +0300

    change string into list[int]

--- src/LeetCode/leet_code_task3.py
+++ src/LeetCode/leet_code_task3.py
@@ -1,11 +1,11 @@
-def find_min_substring_with_size(s: str, k: int) -> str:
-    ans_s = list(s[0])
-    for i in range(1, len(s)):
-        while len(ans_s) > 0 and k and s[i] < ans_s[-1]:
-            ans_s.pop()
+def find_min_substring_with_size(s: str, k: int) -> list[int]:
+    result = [int(s[0])]
+    for digit in s[1:]:
+        while len(result) > 0 and k > 0 and int(digit) < result[-1]:
+            result.pop()
             k -= 1
-        if ans_s or s[i] != "0":
-            ans_s.append(s[i])
+        if len(result) > 0 or digit != "0":
+            result.append(int(digit))
     if k != 0:
-        ans_s = ans_s[: len(ans_s) - k]
-    return "".join(ans_s) or "0"
+        result = result[: len(result) - k]
+    return result or [0]
