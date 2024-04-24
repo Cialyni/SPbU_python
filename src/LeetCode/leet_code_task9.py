abd003b24c311985a748321139804eec8d6c1ed3
Cialyni <kvacheg1002@gmail.com>
2024-04-24 13:13:37 +0300

    black

--- src/LeetCode/leet_code_task9.py
+++ src/LeetCode/leet_code_task9.py
@@ -15,9 +15,19 @@ def longest_univalue_path(root: TreeNode) -> int:
     def _traverse(current_root: TreeNode) -> int:
         if current_root is None or current_root.val is None:
             return 0
-        left_res, right_res = _traverse(current_root.left), _traverse(current_root.right)
-        right = right_res + 1 if current_root.right and current_root.val == current_root.right.val else 0
-        left = left_res + 1 if current_root.left and current_root.val == current_root.left.val else 0
+        left_res, right_res = _traverse(current_root.left), _traverse(
+            current_root.right
+        )
+        right = (
+            right_res + 1
+            if current_root.right and current_root.val == current_root.right.val
+            else 0
+        )
+        left = (
+            left_res + 1
+            if current_root.left and current_root.val == current_root.left.val
+            else 0
+        )
         ans[0] = max(ans[0], left + right)
         return max(left, right)
 
